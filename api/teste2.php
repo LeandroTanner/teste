594d317c43c41eab19427bb4fcc0820cbd07d293
LeandroTanner <151096594+LeandroTanner@users.noreply.github.com>
2023-12-09 00:47:25 -0300

    Create teste2.php

---
+++ api/teste2.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>teste2</title>
+        <meta charset="UTF-8">
+        <style>
+
+        .teste{
+            background-color: blue;
+        }
+
+        </style>
+    </head>
+    <body>
+        
+    <?php
+    
+    echo'<div class="teste">EU SOU DOIDO AAAAAAAAAAAAAAAAAAAAAA</div>';
+    
+    ?>
+
+    <button><a href="./teste.php">voltar</a></button>
+
+    </body>
+</html>
