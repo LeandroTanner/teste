da639ca15caf3147d5ddde42ea8a692a424d71ab
LeandroTanner <151096594+LeandroTanner@users.noreply.github.com>
2023-12-09 00:39:00 -0300

    Add files via upload

---
+++ api/teste.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>teste</title>
+        <meta charset="UTF-8">
+        <style>
+
+        .teste{
+            background-color: red;
+        }
+
+        </style>
+    </head>
+    <body>
+        
+    <?php
+    
+    echo'<div class="teste">LALALALA TESTE</div>';
+    
+    ?>
+
+    </body>
+</html>
